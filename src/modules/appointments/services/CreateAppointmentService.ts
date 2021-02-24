2142adee2641ad5a2a412235ee8f5bdf99f0174f
Lucas <lucasdasilvabarbosa27@gmail.com>
2021-02-24 17:07:35 -0300

    API Backend GoBarbar
    Aplicação finaliza Backend do gosteack gobarber

---
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -0,0 +1,84 @@
+import { startOfHour, isBefore, getHours, format } from 'date-fns';
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import Appointment from '../infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequestDTO {
+  provider_id: string;
+  user_id: string;
+  date: Date;
+}
+
+@injectable()
+class CreateAppointmentService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('NotificationsRepository')
+    private notificationsRepository: INotificationsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
+  ) {}
+
+  public async execute({
+    date,
+    user_id,
+    provider_id,
+  }: IRequestDTO): Promise<Appointment> {
+    const appointmentDate = startOfHour(date);
+
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError('Não é possivel agendar em um horario antigo');
+    }
+
+    if (user_id === provider_id) {
+      throw new AppError('Você não pode criar um agendamento para você mesmo.');
+    }
+
+    if (getHours(appointmentDate) < 7 || getHours(appointmentDate) > 23) {
+      throw new AppError(
+        'Você não pode criar um compromisso fora do horário de trabalho.',
+      );
+    }
+
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
+      appointmentDate,
+      provider_id,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw new AppError('Esse horario já está agendado.');
+    }
+
+    const appointment = await this.appointmentsRepository.create({
+      provider_id,
+      user_id,
+      date: appointmentDate,
+    });
+
+    const dateFormatted = format(appointmentDate, "dd/MM/yyyy 'às' HH:mm'h'");
+
+    await this.notificationsRepository.create({
+      recipient_id: provider_id,
+      content: `Novo agendamento em ${dateFormatted}`,
+    });
+
+    await this.cacheProvider.invalidate(
+      `provider-appointments:${provider_id}:${format(
+        appointmentDate,
+        'yyyy-M-d',
+      )}`,
+    );
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
